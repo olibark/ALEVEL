94eb051dd012cc9b2f7269f41e7c89552628df14
olibarkWin <oliverbarkham@icloud.com>
2025-08-01 17:23:53 +0100

    commit

--- PyGame/PART2ON/main.py
+++ PyGame/PART2ON/main.py
@@ -4,8 +4,8 @@ import constants as c
 (screen, player
 ,enemy, clock) = (init.Init()[0], init.Init()[1],
                   init.Init()[2], init.Init()[3])
-init.drawBG(screen, init.BACKGROUND)
 
+init.drawBG(screen, init.BACKGROUND)
 
 while c.running:
     clock.tick(c.FPS)  # set FPS
