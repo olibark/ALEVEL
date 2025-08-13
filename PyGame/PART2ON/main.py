8a2fe4e6154e64081e9e84d56861e08a27574083
olibark <oliverbarkham@icloud.com>
2025-08-13 19:34:57 +0100

    Handle explosion collisions for grenade damage

--- PyGame/PART2ON/main.py
+++ PyGame/PART2ON/main.py
@@ -1,12 +1,12 @@
 import pygame, init, loop
 import constants as c
 
-(screen, enemies, 
+(screen, enemyGroup,
     player, clock) = init.Init()
 
 while c.running:
     clock.tick(c.FPS)  # set FPS
-    loop.main_loop(player, enemies, screen) #main loopaa
+    loop.main_loop(player, enemyGroup, screen) #main loopaa
 
 pygame.quit()
 
